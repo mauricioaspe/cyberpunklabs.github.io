5239bc74de11d312e6b36c992e9254320a2a8dec
diogenes <diogenes@diogenes-pc>
2020-08-11 09:49:38 -0400

    Mejoras navbar y prueba EJR

--- senalonline.php
+++ senalonline.php
@@ -7,8 +7,6 @@
   <title>El Ágora</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <!-- <meta http-equiv="refresh" content="900" > -->
-    <!-- <link rel="stylesheet" href="css/main.css" /> -->
 </head>
 
 
@@ -18,20 +16,11 @@
 
 
 <figure id="mainPlayer" class="mainPlayer">
-  <?php
-    $mypath = '/home/diogenes/projects/cyberpunklabs.github.io/EJR/';
-    $files  = scandir($mypath);
-    $nfiles = count($files) - 2;
-    $file   = $files[rand(2,$nfiles)];
-    $name   = str_replace("_", " ", $file); 
-    $name   = str_replace("Tape", "/ Tape", $name);
-    $name   = str_replace(" 19", " / 19", $name);
-  ?>
 
   <figcaption><b>The Early Jazz Radio - Señal online</b></figcaption> 
 
   <audio id="audioPlayer" controls autoplay="autoplay" onended="nextSong()">
-  <source src=<?php echo "EJR/".$file?> type="audio/mp3">
+  <source src="EJR/Thelonious_Monk_Tape_5_1971_Side_A.mp3" type="audio/mp3">
 
     Your browser does not support the audio element.
   </audio>
@@ -40,7 +29,7 @@
 
 
   <b>Artista:</b><br>
-    <?php echo substr($name,0,-11)?>
+    Por implementar...
 
   <script>
     var myPlayer = document.getElementById("audioPlayer");
